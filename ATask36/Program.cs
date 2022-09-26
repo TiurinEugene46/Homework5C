fb8b3eace0d056b6ad3d85901bf1946810183139
Evgenii Tiurin <lawyertyurin@gmail.com>
2022-09-26 12:56:44 +0300

    постарался сделать решение к задаче 37

--- ATask36/Program.cs
+++ ATask36/Program.cs
@@ -5,15 +5,18 @@
 int result = 0;
 int N = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите числа: ");
-int[] mass = new int[N];
-for (int i = 0; i < N; i++){
-    mass[i] = Convert.ToInt32(Console.ReadLine());
-}
-for (int i = 0; i < N; i++){
-    if (mass[i] % 2 != 1){
-        result += mass[i];
-    }
-}
-Console.WriteLine($"Сумма нечетных эл.массива составляет {result}");
+            int[] mass = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+               mass[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < N; i++)
+            {
+                if (mass[i] % 2 != 1)
+                {
+                    result += mass[i];
+                }
+            }
+            Console.WriteLine($"Сумма нечетных эл.массива составляет {result}");
 
 // Я не сделал эту задачу
---
+++ ATask37/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21
+*/
+
+static extern
+
+int[] GetPairMult(int[] array);
+{
+    int size = 0;
+    int[] result = null;
+
+    if(array.Length % 2 = 0)
+    {
+        size = array.Length / 2;
+        result = new int[size];
+
+        for(int i = 0; i < size; i++)
+        {
+            result[i] = array[i] * array[array.Length - i - 1];
+        }
+    }
+    else
+    {
+        size = (array.Length / 2) + 1;
+        result = new int[size];
+
+        for(int i = 0; i < size; i++)
+        {
+            if(i == (size - 1))
+            {
+                result[i] = array[i];
+            }
+            else
+            {
+                result[i] = array[i] * array[array.Length - i - 1];
+            }
+        }
+    }
+
+    return result;
+}
+
+Console.WriteLine($"Введите значение массива: ");
+int [] array = new int[](Console.WriteLine);
+int [] result = GetPairMult(array);
+
+// Неверное решение 
